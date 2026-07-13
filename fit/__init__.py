36ad0453553c6b67c1a413e17c45490136a0b8ca
Smith Mary <2534933103a@gmail.com>
2026-07-13 18:50:06 +0800

    chore: remove unused linear backend (fit/linear.py)

--- fit/__init__.py
+++ fit/__init__.py
@@ -4,23 +4,7 @@ from fit.elementwise import fit_elementwise as _fit_elementwise_roofline
 from fit.flashattn import fit_flashattn as _fit_flashattn_roofline
 from fit.cudagraph import fit_cudagraph_all as _fit_cudagraph_all
 from fit.launch_overhead import fit_launch_overhead as _fit_launch_overhead
-from fit.linear import fit_matmul_linear, fit_elementwise_linear
-
-
-def fit_all(results, backend="roofline"):
-    """Run all fits, return combined params dict.
-
-    Args:
-        results: list of benchmark result dicts.
-        backend: "roofline" (default) or "linear".
-    """
-    if backend == "linear":
-        params = {"type": "linear"}
-        params.update(fit_matmul_linear(results))
-        params.update(fit_elementwise_linear(results))
-        return params
-
-    # Default: roofline
+def fit_all(results):
     params = {"type": "roofline"}
     params.update(_fit_matmul_roofline(results))
     params.update(_fit_elementwise_roofline(results))
